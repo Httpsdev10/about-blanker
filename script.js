df440e2ae7013338258cbb2ff6a3e87483978319
Https <koifi1000@gmail.com>
2025-02-04 20:39:46 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+function openInBlank() {
+    let url = document.getElementById("urlInput").value;
+    if (!url.startsWith("http://") && !url.startsWith("https://")) {
+        url = "https://" + url; // Ensure valid URL format
+    }
+    
+    const newWindow = window.open("about:blank", "_blank");
+    if (!newWindow) {
+        alert("Popup blocked! Allow popups for this site.");
+        return;
+    }
+
+    newWindow.document.open();
+    newWindow.document.write(`
+        <!DOCTYPE html>
+        <html lang='en'>
+        <head><title>Embedded Page</title></head>
+        <body style='margin:0;'>
+            <iframe src='${url}' style='width:100vw; height:100vh; border:none;'></iframe>
+        </body>
+        </html>
+    `);
+    newWindow.document.close();
+}
